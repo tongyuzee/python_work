1a45817d99301e5b090798a42c1b52c5af191144
tyztongyuze <1367208449@qq.com>
2019-04-25 16:08:46 +0800

    Chapter 18

---
+++ chapter18/learning_log/ll_en/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!e:\workspace\python_work\chapter18\learning_log\ll_en\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
