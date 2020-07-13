0dcc0b42172e84fee3c05b5f457ec2ec4873569e
tyztongyuze <1367208449@qq.com>
2020-07-13 12:11:46 +0800

    07/13 Anaconda

--- dsp/sig_fft.py
+++ dsp/sig_fft.py
@@ -4,6 +4,23 @@
 # @FileName     : sig_fft.py
 # @Software     : PyCharm
 # --------------
+import numpy as np
+from scipy.fft import fft, ifft
+import matplotlib.pyplot as plt
 
+fs = 4000
+tp = 2
+t = np.linspace(0, tp, tp * fs, endpoint=False)
+
+xt = np.cos(2*np.pi*200*t)
+plt.plot(t, xt)
+plt.show()
+
+nf = 1024
+f = np.linspace(0, fs, nf, endpoint=False)
+xf = fft(xt, nf)
+plt.plot(f, abs(xf))
+plt.show()
+pass
 
 
