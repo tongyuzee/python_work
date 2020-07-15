65b86d656050ac740a46c37ec18ddf80be92b847
tyztongyuze <1367208449@qq.com>
2020-07-15 19:35:39 +0800

    07/15 优化csvread()函数

--- dsp/read_csv.py
+++ dsp/read_csv.py
@@ -8,13 +8,15 @@
 import csv
 
 
-def csvread(filename, start=0):
-    with open(filename, 'r') as fl:
-        reader = csv.reader(fl)
+def csvread(filename, row=0, column=0):
+    """从文件filename的第row行第column列开始读取数据，默认均为0"""
+
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
         sig = list(reader)
-        sig_x = [[float(x) for x in y] for y in sig[start:]]
+        sig_x = [[float(x) for x in y[column:]] for y in sig[row:]]
         return sig_x
 
 
 dat1 = csvread("sig.dat")
-dat2 = csvread("5678EDBC.dat", 1)
+dat2 = csvread("5678EDBC.dat", 1, 0)
