00afef28b4d8a149872d1bd06394a2ec2aa17232
tyztongyuze <1367208449@qq.com>
2020-07-15 18:42:00 +0800

    07/15 模拟matlab环境，对信号做fft得到信号频谱

--- dsp/sig_fft.py
+++ dsp/sig_fft.py
@@ -12,13 +12,17 @@ fs = 4000
 tp = 2
 t = np.linspace(0, tp, tp * fs, endpoint=False)
 xt = np.cos(2*np.pi*200*t)
-plt.subplot(1, 2, 1)
+plt.figure(1)
 plt.plot(t, xt)
 
 nf = 1024
 f = np.linspace(0, fs, nf, endpoint=False)
 xf = fft(xt, nf)
-plt.subplot(1, 2, 2)
-plt.plot(f, abs(xf))
+# 取绝对值并归一化
+xfa = np.abs(xf)/np.amax(np.abs(xf))
+plt.figure(2)
+plt.plot(f[0:nf//2], xfa[0:nf//2])
 plt.show()
-pass
+
+f_out = np.argmax(xfa[0:nf//2])/nf*fs
+print("信号频率为：{:.2f}Hz".format(f_out))
