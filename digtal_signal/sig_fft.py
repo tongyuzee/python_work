61bec60d1a0f3509f69e91a4d0743866aebf7d1a
tyztongyuze <1367208449@qq.com>
2020-07-19 22:43:32 +0800

    07/19 信号fft运算

--- digtal_signal/sig_fft.py
+++ digtal_signal/sig_fft.py
@@ -5,5 +5,26 @@
 # @Software     : PyCharm
 # --------------
 
+import numpy as np
+from scipy.fft import fft
+import matplotlib.pyplot as plt
 
+Fs = 10000
+Tp = 2
+t = np.linspace(0, Tp, Tp * Fs, endpoint=False)
+xt = np.cos(2 * np.pi * 450 * t)
+plt.figure(1)
+plt.plot(t, xt)
 
+NF = 4096
+f = np.linspace(0, Fs, NF, endpoint=False)
+xf = fft(xt, NF)
+xaf = np.abs(xf)/np.amax(np.abs(xf))
+plt.figure(2)
+plt.plot(f[0:NF//2], xaf[0:NF//2])
+plt.show()
+
+f1 = np.argmax(xaf)/NF*Fs
+f2 = np.where(xaf[0:NF//2] == 1)
+print("信号频率为：{:.2f}".format(f1))
+print("信号频率为：{:.2f}".format(f2[0][0]/NF*Fs))
