fa8dfccd5b312a6ee09ff4572c58b5e1bf3f3fe6
tyztongyuze <1367208449@qq.com>
2020-07-26 21:54:01 +0800

    整数排序(int转布尔类型（bool）)

--- algorithm/paixu.py
+++ algorithm/paixu.py
@@ -8,6 +8,7 @@
 @ Date & Time   : 2020/7/26 21:33
 @ Description   : None
 """
+"""bool() 函数用于将给定参数转换为布尔类型，如果没有参数，返回 False。"""
 
 n, x, flag = int(input()), [int(v) for v in input().split()], int(input())
 y = [str(v) for v in sorted(x, reverse=bool(flag))]
