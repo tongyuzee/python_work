5dd47555ba2960e1daadd3ffc8c1f53f504fc41b
tyztongyuze <1367208449@qq.com>
2020-07-31 17:14:35 +0800

    统计选票

---
+++ algorithm/count_votes.py
@@ -0,0 +1,27 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author        : tongyuze
+@ Project       : algorithm
+@ FileName      : count_votes.py
+@ Date & Time   : 2020/7/31 16:23
+@ Description   : 统计选票
+"""
+
+n, candidate = int(input()), input().strip().split()
+
+votes = {'Invalid': 0}
+for i in candidate:
+    votes[i] = 0
+
+m, v = int(input()), input().strip().split()
+for i in v:
+    if i in votes.keys():
+        votes[i] += 1
+    else:
+        votes['Invalid'] += 1
+
+for k in sorted(votes.keys()):
+    if k != 'Invalid':
+        print("{} : {}".format(k, votes[k]))
