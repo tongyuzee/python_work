33c571132cc5f54c8e65bb4f8d311cadec0e22fa
tyztongyuze <1367208449@qq.com>
2020-08-04 17:39:58 +0800

    在字符串中找出连续最长的数字串

---
+++ algorithm/zuichangshuzi.py
@@ -0,0 +1,26 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author        : tongyuze
+@ Project       : algorithm
+@ FileName      : zuichangshuzi.py
+@ Date & Time   : 2020/8/4 17:09
+@ Description   : 在字符串中找出连续最长的数字串
+"""
+
+x = "abcd12345ed125ss123058789"
+a = []
+for v in x:
+    if v.isdecimal():
+        a.append('*' + v + '*')
+    else:
+        a.append(v)
+b = ''.join(a).replace('**', '')
+c = [v for v in b.split('*') if v.isdecimal()]
+d = {len(v): v for v in c}
+m = max(d.keys())
+print("{},{}".format(d[m], m))
+
+
+
