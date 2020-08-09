eb1f937a2055122f1f493ef3d1d4224b43434ce2
tyztongyuze <1367208449@qq.com>
2020-08-09 22:25:40 +0800

    删除最少的字符

--- algorithm/delstr.py
+++ algorithm/delstr.py
@@ -12,3 +12,17 @@
 实现删除字符串中出现次数最少的字符，若多个字符出现次数一样，则都删除。
 输出删除这些单词后的字符串，字符串中其它字符保持原来的顺序。
 """
+
+s = list(input())
+d = {}
+for v in set(s):
+    c = s.count(v)
+    if c in d.keys():
+        d[c] += v
+    else:
+        d[c] = v
+p = min(d.keys())
+for i in d[p]:
+    while i in s:
+        s.remove(i)
+print(''.join(s))
