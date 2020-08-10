c436bd3f43e5524bdbf2177322ff404bdeb60950
tyztongyuze <1367208449@qq.com>
2020-08-10 21:01:07 +0800

    斐波那契数列

--- algorithm/fibonacci.py
+++ algorithm/fibonacci.py
@@ -11,11 +11,11 @@
 
 
 def fib(n):
-    if n in [1, 2]:
+    if n == 1:
         return 1
     else:
-        a, b = 1, 1
-        for i in range(3, n+1):
+        a, b = 0, 1
+        for i in range(2, n+1):
             a, b = b, a+b
         return b
 
