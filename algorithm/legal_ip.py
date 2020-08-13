1c2baf37c35e741fc3eda7308335999386d54854
tyztongyuze <1367208449@qq.com>
2020-08-13 16:59:35 +0800

    合法IP

---
+++ algorithm/legal_ip.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author        : tongyuze
+@ Project       : algorithm
+@ FileName      : legal_ip.py
+@ Date & Time   : 2020/8/13 16:59
+@ Description   : 合法IP
+"""
