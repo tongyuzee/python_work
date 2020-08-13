ad0e414cdbef27e383b955573ff6c391406732dc
tyztongyuze <1367208449@qq.com>
2020-08-13 17:07:15 +0800

    合法IP

--- algorithm/legal_ip.py
+++ algorithm/legal_ip.py
@@ -8,3 +8,18 @@
 @ Date & Time   : 2020/8/13 16:59
 @ Description   : 合法IP
 """
+
+
+def islegal(ip):
+    """判断ipv4的IP是否合法"""
+    x = ip.split('.')
+    if len(x) != 4:
+        return 'NO'
+    else:
+        for v in x:
+            if not v.isdecimal() or int(v) > 255:
+                return 'NO'
+        return 'YES'
+
+
+print(islegal(input()))
