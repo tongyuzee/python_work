e894ab1b44596afc1d06f62cddada6822b3305f6
tyztongyuze <1367208449@qq.com>
2020-08-17 23:06:20 +0800

    冒泡排序

--- algorithm/maopo.py
+++ algorithm/maopo.py
@@ -8,3 +8,12 @@
 @ FileName     : maopo.py
 @ Description  : 冒泡排序
 """
+
+a = [int(_) for _ in input().split()]
+for i in range(len(a)):
+    for j in range(1, len(a)-i):
+        if a[i] > a[j]:
+            temp = a[j]
+            a[j] = a[i]
+            a[i] = temp
+print(' '.join([str(_) for _ in a]))
