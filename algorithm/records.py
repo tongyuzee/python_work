5ad170dc998e213a58fead6a2450c70118a1c03a
tyztongyuze <1367208449@qq.com>
2020-08-20 16:20:14 +0800

    简单错误记录

---
+++ algorithm/records.py
@@ -0,0 +1,10 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author        : tongyuze
+@ Project       : algorithm
+@ FileName      : records.py
+@ Date & Time   : 2020/8/20 16:19
+@ Description   : 简单错误记录
+"""
