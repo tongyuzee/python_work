00d7bc10a05bb7b218f3be128d13fb369792b6b1
tyztongyuze <1367208449@qq.com>
2020-08-20 17:26:41 +0800

    简单错误记录

--- algorithm/records.py
+++ algorithm/records.py
@@ -8,3 +8,21 @@
 @ Date & Time   : 2020/8/20 16:19
 @ Description   : 简单错误记录
 """
+r = []
+while True:
+    s = input().replace('\\', '/')
+    if s == '':
+        break
+    else:
+        r.append(s)
+d = {}
+for v in r[-8:]:
+    u = v.split()
+    p = u[0].split('/')
+    e = p[-1][-16:] + ' ' + u[-1]
+    if e in d.keys():
+        d[e] += 1
+    else:
+        d[e] = 1
+for k, v in d.items():
+    print("{} {}".format(k, v))
