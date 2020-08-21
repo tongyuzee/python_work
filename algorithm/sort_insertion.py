3398cc57f1e9bc67e8d3ab99d2595d46f055baf1
tyztongyuze <1367208449@qq.com>
2020-08-21 10:29:44 +0800

    插入排序

--- algorithm/sort_insertion.py
+++ algorithm/sort_insertion.py
@@ -8,3 +8,12 @@
 @ Date & Time   : 2020/8/21 10:21
 @ Description   : 插入排序
 """
+
+a = [int(_) for _ in input().split()]
+for i in range(1, len(a)):
+    key = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > key:
+        a[j + 1] = a[j]
+        j -= 1
+    a[j + 1] = key
