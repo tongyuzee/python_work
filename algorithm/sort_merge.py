25c1d6331bf78e7326c4369a2d6a8f8cbe2c288a
tyztongyuze <1367208449@qq.com>
2020-08-21 17:42:35 +0800

    合并递归排序

--- algorithm/sort_merge.py
+++ algorithm/sort_merge.py
@@ -11,12 +11,16 @@
 
 
 def merge(left, right):
+    # 如果有一个数组为空，那么不需要合并，
+    # 可以直接将另一个数组返回作为结果
     if len(left) == 0:
         return right
     if len(right) == 0:
         return left
+
     result = []
     i_left, i_right = 0, 0
+    # 查看两个数组直到所有元素都装进结果数组中
     while len(result) <= len(left) + len(right):
         if left[i_left] < right[i_right]:
             result.append(left[i_left])
@@ -24,6 +28,9 @@ def merge(left, right):
         else:
             result.append(right[i_right])
             i_right += 1
+
+        # 如果哪个数组达到了最后一个元素，那么可以将另外一个数组的剩余元素
+        # 装进结果列表中，然后终止循环
         if i_left >= len(left):
             result += right[i_right:]
             break
@@ -34,14 +41,16 @@ def merge(left, right):
 
 
 def merge_sort(arry):
+    # 如果输入数组包含元素不超过两个，那么返回它作为函数结果
     if len(arry) < 2:
         return arry
 
     midpoint = len(arry) // 2
 
+    # 对数组递归地划分为两部分，排序每部分然后合并装进最终结果列表
     return merge(merge_sort(arry[:midpoint]), merge_sort(arry[midpoint:]))
 
 
-a = [8, 2, 6, 4, 5]
+a = [8, 6, -4]
 r = merge_sort(a[:])
 print(' '.join([str(_) for _ in r]))
