da8fdd1c64468ebbe1be50a3cb701d65d3b3c2b4
tyztongyuze <1367208449@qq.com>
2020-08-22 22:11:59 +0800

    快速排序

--- algorithm/sort_quick.py
+++ algorithm/sort_quick.py
@@ -11,3 +11,21 @@
 
 
 def quicksort(arry):
+    if len(arry) < 2:
+        return arry
+    else:
+        key = arry[len(arry)//2]
+        little, equal, large = [], [], []
+        for v in arry:
+            if v < key:
+                little.append(v)
+            elif v == key:
+                equal.append(v)
+            else:
+                large.append(v)
+        return quicksort(little) + equal + quicksort(large)
+
+
+a = [8, 6, -4, 9, 10]
+r = quicksort(a)
+print(' '.join([str(_) for _ in r]))
