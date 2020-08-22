ca5d5c97bcaa04f5c08beccf394912dfca82ca28
tyztongyuze <1367208449@qq.com>
2020-08-22 22:05:19 +0800

    快速排序

---
+++ algorithm/sort_quick.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author       : tongyuze
+@ Project      : algorithm
+@ Date & Time  : 2020/8/22 22:03
+@ FileName     : sort_quick.py
+@ Description  : 快速排序
+"""
+
+
+def quicksort(arry):
