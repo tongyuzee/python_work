22150baa9e99d86811d2f4cc3d761767ad72ce8d
tyztongyuze <1367208449@qq.com>
2020-08-23 21:59:41 +0800

    二维列表

---
+++ algorithm/2_dimension.py
@@ -0,0 +1,13 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author       : tongyuze
+@ Project      : algorithm
+@ Date & Time  : 2020/8/23 21:56
+@ FileName     : 2_dimension.py
+@ Description  : 二维列表
+"""
+
+b = [['-'] * 3 for _ in range(3)]
+b[1][2] = 'X'
