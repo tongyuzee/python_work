6c0516e2035479442eec800382de3ff84b06389a
tyztongyuze <1367208449@qq.com>
2020-08-24 22:34:21 +0800

    二分查找（bisect）

--- algorithm/search.py
+++ algorithm/search.py
@@ -19,16 +19,22 @@ b = input("Enter 'l' or 'r:").lower()
 if b == 'l':
     # bisect_left 返回的插入位置是原序列中和被插入元素相等的元素的位置，即新元素被放在与它相等的元素之前
     bisect_fn = bisect.bisect_left
+    bisect_in = bisect.insort_left
 else:
     # bisect.bisect = bisect.bisect_right
     # bisect_right 返回的插入位置是原序列中和被插入元素相等的元素之后的位置，即新元素被放在与它相等的元素之后
     bisect_fn = bisect.bisect_right
+    bisect_in = bisect.insort_right
 
 print('DEMO:', bisect_fn.__name__)
 print('haystack ->', ' '.join('{:2d}'.format(h) for h in haystack))
-
 row_fmt = '{0:2d} @ {1:2d}    {2}{0:<2d}'
 for n in sorted(needles, reverse=True):
     position = bisect_fn(haystack, n)
     offset = '  |' * position
     print(row_fmt.format(n, position, offset))
+
+print('DEMO:', bisect_in.__name__)
+for n in needles:
+    bisect_in(haystack, n)
+    print("{:2d}".format(n), haystack)
