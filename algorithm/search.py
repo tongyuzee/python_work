fc1d7af84f211cba2fcdef5c5f77b17fa214fdd3
tyztongyuze <1367208449@qq.com>
2020-08-24 21:58:02 +0800

    二分查找（bisect）

---
+++ algorithm/search.py
@@ -0,0 +1,14 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author       : tongyuze
+@ Project      : algorithm
+@ Date & Time  : 2020/8/24 21:53
+@ FileName     : search.py
+@ Description  : 二分查找（bisect）
+"""
+import bisect
+
+h = [1, 4, 5, 6, 8, 10, 15, 17, 18, 19, 25, 30, 31]
+n = [0, 1, 24, 30, 5, 8, 58, 32, 6, 17]
