2ea2beb384c55716e0667d138fb09fda922ff4ab
tyztongyuze <1367208449@qq.com>
2020-08-25 23:14:23 +0800

    二分查找

--- algorithm/search_2.py
+++ algorithm/search_2.py
@@ -8,3 +8,12 @@
 @ Date & Time   : 2020/8/25 17:02
 @ Description   : 二分查找
 """
+
+
+def search2(n, v, a):
+    midp = (n + 1) // 2
+    if a[midp] == v:
+        temp = midp
+        while a[temp-1] == v:
+            temp -= 1
+        r
