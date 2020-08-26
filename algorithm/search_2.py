c64709e7fdb0d19aea37cf5957260dbd415da358
tyztongyuze <1367208449@qq.com>
2020-08-26 11:20:35 +0800

    二分查找

--- algorithm/search_2.py
+++ algorithm/search_2.py
@@ -11,9 +11,20 @@
 
 
 def search2(n, v, a):
-    midp = (n + 1) // 2
+
+    if n == 0:
+        return -1
+
+    midp = n // 2
     if a[midp] == v:
-        temp = midp
-        while a[temp-1] == v:
-            temp -= 1
-        r
+        while a[midp - 1] == v:
+            midp -= 1
+        return midp
+    elif a[midp] > v:
+        return search2(midp, v, a[:midp])
+    else:
+        return search2(n-midp-1, v, a[midp+1:])
+
+
+x = search2(6, 4, [1, 4, 4, 4, 5, 6])
+print(x)
