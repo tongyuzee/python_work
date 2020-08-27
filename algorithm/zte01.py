7fee95d0fa2b83c2986e0e075dbdca57efb01e31
tyztongyuze <1367208449@qq.com>
2020-08-27 10:51:41 +0800

    矩阵十字

--- algorithm/zte01.py
+++ algorithm/zte01.py
@@ -13,6 +13,10 @@ m, n = [int(_) for _ in input().split()]
 A = []
 for i in range(m):
     A.append([int(_) for _ in input().split()])
-
+ans = 0
 for i in range(1, m-1):
     for j in range(1, n-1):
+        res = A[i][j] + A[i-1][j] + A[i+1][j] + A[i][j-1] + A[i][j+1]
+        if res > ans:
+            ans = res
+print(ans)
