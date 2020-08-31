6a0e13421a62512654246123a1a75803c1a18e82
tyztongyuze <1367208449@qq.com>
2020-08-31 23:01:57 +0800

    signal

---
+++ algorithm/signal.py
@@ -0,0 +1,15 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author       : tongyuze
+@ Project      : algorithm
+@ Date & Time  : 2020/8/31 22:59
+@ FileName     : signal.py
+@ Description  : None
+"""
+import numpy
+
+x = [int(_) for _ in input().split()]
+a = numpy.array(x)
+
