96e0fe2d5247c05cc1e6cf50938e2f8414a71032
tyztongyuze <1367208449@qq.com>
2020-09-03 13:46:32 +0800

    clean

---
+++ hahaha0903/base_conversion.py
@@ -0,0 +1,35 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+@ Author       : tongyuze
+@ Project      : ceshi0903
+@ Date & Time  : 2020/9/3 13:08
+@ FileName     : base_conversion.py
+@ Description  : 禁止转换
+"""
+
+
+def conv10_n(m, n):
+    m = int(m)
+    n = int(n)
+    r = []
+    while m // n != 0:
+        r.append(str(m % n))
+        m = m // n
+    r.append(str(m))
+    return ''.join(reversed(r))
+
+
+def convn_10(m, n):
+    n = int(n)
+    r = 0
+    for i, v in enumerate(reversed(list(m))):
+        r += int(v) * (n ** i)
+    return str(r)
+
+
+x, y = input().split()
+z = conv10_n(x, y)
+print(z)
+print(convn_10(z, y))
