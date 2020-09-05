424fd0e6051e2fb560becbbceff814b94fb16d51
tyztongyuze <1367208449@qq.com>
2020-09-05 20:31:11 +0800

    数组array

---
+++ Fluent_Py/2.9array.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@ Author        : tongyuze
+@ Project       : Fluent_Py
+@ Date & Time   : 2020/9/5 18:57
+@ FileName      : 2.9array.py
+@ Description   : None
+"""
+import array
+
+floats = array.array('d', [1, 2, 3, 4])
+print(list(floats))
+fp = open('floats.bin', 'wb')
+floats.tofile(fp)
+fp.close()
+floats2 = array.array('d')
+fp = open("floats.bin", 'rb')
+floats2.fromfile(fp, 4)
+print(list(floats2))
