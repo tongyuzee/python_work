fdb5feb669d5c84e5d5b28924ee95ad3844c17ad
tyztongyuze <1367208449@qq.com>
2020-09-15 22:20:25 +0800

    bianli

--- hahaha0903/a002.py
+++ hahaha0903/a002.py
@@ -10,30 +10,28 @@
 """
 
 
-class Convdate:
+class Date:
 
     def __init__(self, s):
-        ss = ''
-        for v in s:
-            if not v.isdecimal():
-                ss += '*'
-            else:
-                ss += v
-        self.shuzi = ss.split('*')
-        self.shuzi.pop()
+        self.date = {}
+        j = 0
+        for i, v in enumerate(s):
+            if v in '年月日时分秒':
+                self.date[v] = s[j:i]
+                j = i+1
 
     def pri0(self):
-        print('.'.join(self.shuzi[:3]), ':'.join(self.shuzi[-3:]))
+        print('.'.join(list(self.date.values())[:3]), ':'.join(list(self.date.values())[-3:]))
 
     def pri1(self):
-        print('-'.join(self.shuzi[:3]), '-'.join(self.shuzi[-3:]))
+        print('-'.join(list(self.date.values())[:3]), '-'.join(list(self.date.values())[-3:]))
 
     def pri2(self):
-        print(''.join(self.shuzi[:3]), ''.join(self.shuzi[-3:]))
+        print(''.join(list(self.date.values())[:3]), ''.join(list(self.date.values())[-3:]))
 
 
 x = '2020年09月03日16时34分40秒'
-a = Convdate(x)
+a = Date(x)
 a.pri0()
 a.pri1()
 a.pri2()
